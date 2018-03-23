f4ba2209277930bfa4f7a48879e8ae94e7e848a7
Andrew Rhodes <arhodes5@mix.wvu.edu>
2018-03-23 11:15:22 -0400

    add external code

---
+++ MATLAB/ext/MeshLP/mexcommands.m
@@ -0,0 +1,11 @@
+
+
+
+
+% MEX symmshlpmatrix.C
+mex -v -I/Documents/Software/MeshLP/geodesics 'GCC=/usr/bin/gcc-4.7' symmshlpmatrix.C tmesh.C point.C matrix.C offobj.C comp_meshlpmatrix.C meshlpmatrix.C
+
+
+
+% MEX cotlpmatrix.C
+mex -v -I/Documents/Software/MeshLP/geodesics 'GCC=/usr/bin/gcc-4.7' cotlpmatrix.C tmesh.C point.C matrix.C offobj.C comp_meshlpmatrix.C meshlpmatrix.C
