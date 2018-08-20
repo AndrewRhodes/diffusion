9da72c70a8fd62132222f94fa7868b687f495b16
Andrew Rhodes <arhodes5@mix.wvu.edu>
2018-08-20 17:39:49 -0400

    Itokawa keypoint data

---
+++ MATLAB/main/DE/keypointdata/itokawa/DE_Itokawa_e1_04.m
@@ -0,0 +1,199 @@
+% Andrew Rhodes
+% ASEL
+% March 2018
+
+
+close all
+clear
+clc
+
+global ProjectRoot; % Additional Paths
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% User Defined Criteria
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+alpha = 1;
+
+options.rho = 4;
+options.dtype = 'geodesic';
+% options.dtype = 'euclidean';
+
+ShowPlot = 0;
+Model = 'itokawa/Itokawa_e1_80000';
+
+BDF = 2;
+tauFraction = 1/10;
+NumIter = 50;
+tauNumerator = 250;
+DoGNormalize = 'DoG'; % 'DoG', 'AbsDoG', 'NLoG', 'AbsNLoG'
+CompareMethod = '<>'; % '<', '>', '<>'
+KeypointMethod = 'Old'; % 'Old', 'New'
+
+
+
+
+NoiseVec = [0.4];
+
+t_scale = 0.7;
+t_DoG = 0.9;
+t_range = 3;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Model File Location
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+FileLocationModel = strcat(ProjectRoot,'/models/object/');
+FileNameModelPly = strcat(Model,'.ply');
+FileNameModelOff = strcat(Model,'.off');
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Load the Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[PointCloud.Location, PointCloud.Face] = read_ply( fullfile( FileLocationModel, FileNameModelPly ) );
+
+
+PointCloud.LocationCount = size(PointCloud.Location,1);
+PointCloud.FaceCount = size(PointCloud.Face, 1);
+PointCloud.FaceArea = findFaceArea(PointCloud.Location,PointCloud.Face);
+PointCloud = findMeshResolution(PointCloud, 'Model');
+PointCloud = findMeshNormals(PointCloud)
+% NormalRotations = findNormalsRotation(PointCloud.Normal);
+
+
+% % % % % % % % % %
+tau = PointCloud.Resolution * tauFraction;
+MaxTau = tauNumerator / PointCloud.Resolution;
+NumSteps = round(MaxTau);
+NumSteps = tauNumerator;
+% % % % % % % % % %
+
+load('ItokawaCurvature_e1_80000.mat')
+MK = Curvature;
+
+% [Neighbors, NeighborFaces, PointCloud] = findAdjacentNeighbors(PointCloud);
+% save Itokawa_e1_80000_Neighbors Neighbors
+load('Itokawa_e1_80000_Neighbors.mat','Neighbors')
+PointCloud = findLocalResolution(PointCloud, Neighbors.Connect);
+
+
+% [PK1, PK2, PD1, PD2, MK, GK] = findPointCurvatures(PointCloud, NormalRotations, Neighbors.Connect);
+% clear PK1 PK2 PD1 PD2 GK NeighborFaces NormalRotations
+stdMK = std(MK);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Setup Laplace-Beltrami
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%ItL = makeExplicitLaplaceBeltrami( fullfile( FileLocationModel, FileNameModelOff ), options, BDF, tau, alpha);
+
+%save ItokawaItL_e1_80000 ItL
+load('ItokawaItL_e1_80000.mat')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Scale Parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+ScaleParameter = findScaleParamter(tau, alpha, NumSteps, 'Laplacian', 'Natural');
+
+ScaleParameterAbsolute = bsxfun(@plus, ScaleParameter, PointCloud.Resolution);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Diffusion of Mean Curvature
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for j = 1 : length(NoiseVec)
+    
+   for i = 20:50
+        i
+        PointCloud.Signal = MK + NoiseVec(j)*stdMK*rand(PointCloud.LocationCount,1);
+        
+        
+        Signal = performBDFDiffusion(PointCloud.Signal, NumSteps, ItL);
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Find Difference of Gaussian
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        DoG = buildDoG(Signal, ScaleParameter, DoGNormalize);
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Detect Extrema
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        Keypoint = findKeypoint(DoG, PointCloud, ScaleParameter, Neighbors.Distance, KeypointMethod, CompareMethod);
+        NMSKeypoint = applyNMS(PointCloud, DoG, Keypoint, t_scale, t_range, DoGNormalize, CompareMethod);
+        
+
+        % SubKeypoint = findSubKeypoint(Keypoint, ScaleParameterAbsolute, DoG, PointCloud, Neighbors, NeighborFaces);
+        
+        
+        FileLocation = strcat(ProjectRoot,'/main/DE/keypointdata/itokawa/Std_',num2str(NoiseVec(j)),'/');
+        FileName = strcat('Keypoint','_Iter',num2str(i),'.mat');
+%              FileName = strcat('Keypoint','.mat');
+        
+        save(fullfile(FileLocation, FileName), 'Keypoint', '-v7.3')
+        
+        
+        FileName = strcat('NMSKeypoint','_Iter',num2str(i),'.mat');
+%              FileName = strcat('NMSKeypoint','.mat');
+        
+        save(fullfile(FileLocation, FileName), 'NMSKeypoint', '-v7.3')
+        
+        
+   end
+    
+end
+
+
+
+for i = 1
+    
+    PointCloud.Signal = MK;
+    
+    Signal = performBDFDiffusion(PointCloud.Signal, NumSteps, ItL);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Find Difference of Gaussian
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    DoG = buildDoG(Signal, ScaleParameter, DoGNormalize);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Detect Extrema
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    Keypoint = findKeypoint(DoG, PointCloud, ScaleParameter, Neighbors.Distance, KeypointMethod, CompareMethod);
+        NMSKeypoint = applyNMS(PointCloud, DoG, Keypoint, t_scale, t_range, DoGNormalize, CompareMethod);
+    
+    %     SubKeypoint = findSubKeypoint(Keypoint, ScaleParameterAbsolute, DoG, PointCloud, Neighbors.Connect, NeighborFaces.Connect);
+    
+    for j = 1 : length(NoiseVec)
+        FileLocation = strcat(ProjectRoot,'/main/DE/keypointdata/itokawa/Std_',num2str(NoiseVec(j)));
+        FileName = strcat('Keypoint','.mat');
+
+        save(fullfile(FileLocation, FileName), 'Keypoint', '-v7.3')
+
+        FileName = strcat('NMSKeypoint','.mat');
+        save(fullfile(FileLocation, FileName), 'NMSKeypoint', '-v7.3')
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
